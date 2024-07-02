2becd04c46ccbbd995dc537836db170c7f84773f
lucadisanto7 <lucadisanto47@gmail.com>
2024-07-02 15:24:38 +0200

    [UPD] Added while cycle

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@
  // RECUPERO L'ELEMENTO CHE DEVE CONTENERE LA MIA LISTA DELLA SPESA
  const lista_spesa = document.getElementById('lista-spesa');
 
+ // CICLO L'ARRAY
+ let i = 0;
+ while (i < ingredienti.length) {
+     // CREO UN NUOVO LIST ITEM
+     const li = document.createElement('li');
+
+     // GLI METTO IL TESTO ALL'INTERNO
+     li.innerText = ingredienti[i];
+
+     // APPENDO IL LIST ITEM ALL'INTERNO DELLA LISTA DELLA SPESA
+     lista_spesa.append(li);
+
+     // INCREMENTO LA VARIABILE DI INDICE
+     i++;
+ }
