c15fca3fa0e9997803da0809aa412e7c23bf72ad
lucadisanto7 <lucadisanto47@gmail.com>
2024-07-02 15:24:24 +0200

    [UPD] Declared const ingredienti and lista della spesa

---
+++ js/script.js
@@ -0,0 +1,6 @@
+ // HO UNA LISTA DELLA SPESA, DEVO MOSTRARLA A VIDEO
+ const ingredienti = ['Guanciale', 'Peperoni', 'Pasta', 'Olive', 'Controfiletto', 'Quaglie', 'Insalata', 'Gamberi', 'Pollo', 'Pomodori', 'Melanzane'];
+
+ // RECUPERO L'ELEMENTO CHE DEVE CONTENERE LA MIA LISTA DELLA SPESA
+ const lista_spesa = document.getElementById('lista-spesa');
+
